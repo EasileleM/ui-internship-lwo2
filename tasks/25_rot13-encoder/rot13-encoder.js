71e3495a8326c78180e8101441abb3bb5f749185
EasileleM <pro100prosavva@gmail.com>
2019-09-11 23:23:40 +0400

    small refactoring

--- tasks/25_rot13-encoder/rot13-encoder.js
+++ tasks/25_rot13-encoder/rot13-encoder.js
@@ -1,15 +1,18 @@
-export function rot13Encoder(str) {
+export function rot13Encoder(arr) {
   const isLetter = /^[A-Za-z]+$/;
-  str = str.split('');
-  str.forEach((letter, charIndex) => {
-    if (isLetter.test(letter)) {
-      const curLetterCode = str[charIndex].charCodeAt(0);
-      if (isLetter.test(String.fromCharCode(curLetterCode - 13))) {
-        str[charIndex] = String.fromCharCode(curLetterCode - 13);
-      } else {
-        str[charIndex] = String.fromCharCode(curLetterCode + 13);
-      }
-    }
-  });
-  return str.join('');
+  return arr
+      .split('')
+      .map((char) => {
+        if (isLetter.test(char)) {
+          const curLetterCode = char.charCodeAt(0);
+          if (isLetter.test(String.fromCharCode(curLetterCode - 13))) {
+            return String.fromCharCode(curLetterCode - 13);
+          } else {
+            return String.fromCharCode(curLetterCode + 13);
+          }
+        }
+        return char;
+      })
+      .join('');
 }
+
