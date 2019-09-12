ff06fe4e69f8a353f774c805571c391e9d3cd69b
EasileleM <pro100prosavva@gmail.com>
2019-09-12 23:17:07 +0400

    *feat: add task 35

---
+++ tasks/35_array-leaders/array-leaders.js
@@ -0,0 +1,7 @@
+export function arrayLeaders(arr) {
+  const result = [];
+  for (let i = arr.length - 1, sum = 0; i >= 0; sum += arr[i], i--) {
+    if (arr[i] > sum) result.push(arr[i]);
+  }
+  return result.reverse();
+}
