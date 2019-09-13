674663f698e4076bf4572013bfc39e36f526d29c
EasileleM <pro100prosavva@gmail.com>
2019-09-13 12:45:56 +0400

    *refactor: made it more readable

--- tasks/35_array-leaders/array-leaders.js
+++ tasks/35_array-leaders/array-leaders.js
@@ -1,9 +1,11 @@
 export function arrayLeaders(arr) {
   const result = [];
-  for (let i = arr.length - 1, sum = 0; i >= 0; sum += arr[i], i--) {
+  let sum = arr.reduce((sum, value) => sum + value);
+  for (let i = 0; i < arr.length; i++) {
+    sum -= arr[i];
     if (arr[i] > sum) {
       result.push(arr[i]);
     }
   }
-  return result.reverse();
+  return result;
 }
