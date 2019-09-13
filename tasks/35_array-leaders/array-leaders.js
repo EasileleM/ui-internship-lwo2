7a52791f3ae8b6cad242aee7eace44345e617ea8
EasileleM <pro100prosavva@gmail.com>
2019-09-13 10:21:54 +0400

    *refactor: added curly braces to if operator

--- tasks/35_array-leaders/array-leaders.js
+++ tasks/35_array-leaders/array-leaders.js
@@ -1,7 +1,9 @@
 export function arrayLeaders(arr) {
   const result = [];
   for (let i = arr.length - 1, sum = 0; i >= 0; sum += arr[i], i--) {
-    if (arr[i] > sum) result.push(arr[i]);
+    if (arr[i] > sum) {
+      result.push(arr[i]);
+    }
   }
   return result.reverse();
 }
