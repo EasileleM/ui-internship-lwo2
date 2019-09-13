3b68f27908783400bd6848553a8f738499eaa397
EasileleM <pro100prosavva@gmail.com>
2019-09-13 10:20:22 +0400

    *refactor: added spaces around % operator

--- tasks/36_row-weight/row-weight.js
+++ tasks/36_row-weight/row-weight.js
@@ -1,7 +1,7 @@
 export function rowWeights(arr) {
   const result = [0, 0];
   arr.forEach((value, index) => {
-    if (index%2) {
+    if (index % 2) {
       result[1] += value;
     } else {
       result[0] +=value;
