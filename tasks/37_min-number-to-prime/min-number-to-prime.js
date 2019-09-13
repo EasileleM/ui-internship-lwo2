aa4a4f94ccc202375de08b2672cb0b052ba8e8d7
EasileleM <pro100prosavva@gmail.com>
2019-09-13 12:42:17 +0400

    *refactor: isPrime now out of its parent

--- tasks/37_min-number-to-prime/min-number-to-prime.js
+++ tasks/37_min-number-to-prime/min-number-to-prime.js
@@ -1,13 +1,14 @@
+const isPrime = (num) => {
+  for (let i = 2, numSqrt = Math.sqrt(num); i < numSqrt; i++) {
+    if (!(num % i)) {
+      return false;
+    }
+  }
+  return true;
+};
+
 export function minNumberToPrime(arr) {
   const sum = arr.reduce((sum, value) => sum += value);
-  const isPrime = (num) => {
-    for (let i = 2, numSqrt = Math.sqrt(num); i < numSqrt; i++) {
-      if (!(num % i)) {
-        return false;
-      }
-    }
-    return true;
-  };
   let result = 0;
   while (!isPrime(sum + result)) {
     result++;
