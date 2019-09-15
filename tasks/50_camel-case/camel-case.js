3b2723ac9f68840f35e853f7634fc92ff27c0ab6
EasileleM <pro100prosavva@gmail.com>
2019-09-15 16:22:02 +0400

    *feat: Task 50 initial

--- tasks/50_camel-case/camel-case.js
+++ tasks/50_camel-case/camel-case.js
@@ -1,11 +1,9 @@
+// eslint-disable-next-line no-extend-native
 String.prototype.camelCase = function() {
   return this
       .toLowerCase()
       .split(' ')
-      .map((word, index) => {
-        if (!index) {
-          return word;
-        }
+      .map((word) => {
         return word.charAt(0).toUpperCase() + word.slice(1);
       })
       .join('');
