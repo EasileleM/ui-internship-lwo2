10176d52124f91e6c616ef984ea027fb1e5756bf
EasileleM <pro100prosavva@gmail.com>
2019-09-15 16:57:17 +0400

    *fix: logic repair - 48

--- tasks/48_follow-the-spy/follow-the-spy.js
+++ tasks/48_follow-the-spy/follow-the-spy.js
@@ -1,9 +1,15 @@
 export function followTheSpy(arr) {
-  let currentRoute = arr[0];
-  let result = [arr[0][0]];
-  while (currentRoute) {
-    result.push(currentRoute[1]);
-    currentRoute = arr.find((value) => value[0] === currentRoute[1]);
+  let index = 0;
+  let result = [];
+  const arrLen = arr.length;
+  while ((result.length - 1 !== arrLen) && index < arrLen) {
+    let currentRoute = arr[index];
+    result = [arr[index][0]];
+    while (currentRoute) {
+      result.push(currentRoute[1]);
+      currentRoute = arr.find((value) => value[0] === currentRoute[1]);
+    }
+    index++;
   }
   return result.join(', ');
 }
