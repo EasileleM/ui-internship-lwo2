5d5845af88f0eb30e4471220082885374fef51be
EasileleM <pro100prosavva@gmail.com>
2019-09-16 19:01:47 +0400

    *refactor: reduced amount of operations

--- tasks/47_string-expansion/string-expansion.js
+++ tasks/47_string-expansion/string-expansion.js
@@ -1,17 +1,14 @@
 export function stringExpansion(str) {
-  const words = str.split(/[0-9]+/g);
-  // this regex leads to one useless array element "" with 0 index
-  const repeatitions = str.split(/[A-Za-z]+/g);
-  // this regex leads to one useless array element ""
-  // with repetition.lenght - 1 index
-  // it cause these magic numbers in the loop below
-  const amountOfWords = words.length - 1;
-  for (let i = 0; i < amountOfWords; i++) {
-    const currentWord = words[i+1];
-    words[i + 1] = '';
-    for (const letter of currentWord) {
-      words[i + 1] += letter.repeat(repeatitions[i] % 10);
+  let result = '';
+  const checkNumber = /^\d+$/;
+  let repetitions = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (checkNumber.test(str.charAt(i))) {
+      repetitions = Number(str.charAt(i));
+    } else {
+      result += str.charAt(i).repeat(repetitions);
     }
   }
-  return words.join('');
+  return result || str;
 }
+
