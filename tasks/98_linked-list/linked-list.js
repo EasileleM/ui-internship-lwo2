e261d8e23b0f25f9322cb52a565234cb52f0d86b
EasileleM <pro100prosavva@gmail.com>
2019-09-22 20:39:36 +0400

    *docs: add docs to tasks 98 and 99

--- tasks/98_linked-list/linked-list.js
+++ tasks/98_linked-list/linked-list.js
@@ -1,3 +1,16 @@
+/**
+ * LinkedList - module, that implements linked list behaviour.
+ * Return API that contains such methods and properties:
+ * push()
+ * pop()
+ * iterate()
+ * removeItem()
+ * insertAfter()
+ * amount - amount of nodes in the list
+ * head - referrence to head of the list
+ * tail - referrence to tail of the list
+ * @return {Object} API
+ */
 export function LinkedList() {
   const api = {
     push: push,
@@ -12,6 +25,13 @@ export function LinkedList() {
   let head = null;
   let tail = null;
   let amount = 0;
+  /**
+   * insertAfterNode - service function, that inserts
+   * creates new node with passed value and inserts
+   * it after passed node
+   * @param {Object} node
+   * @param {*} value
+   */
   function insertAfterNode(node, value) {
     node.next = {
       value: value,
@@ -24,7 +44,12 @@ export function LinkedList() {
     amount++;
     updateAPI();
   }
-  function remove(node) {
+  /**
+   * removeNode - service function, that
+   * removes passed node from the list
+   * @param {Object} node
+   */
+  function removeNode(node) {
     if (!node.prev) {
       head = node.next;
       if (!head) {
@@ -41,6 +66,12 @@ export function LinkedList() {
     amount--;
     updateAPI();
   }
+  /**
+   * find - service function, that return
+   * node with passed value or null if it is absent
+   * @param {*} value
+   * @return {Object}
+   */
   function find(value) {
     let currNode = head;
     while (currNode !== null && currNode.value !== value) {
@@ -48,11 +79,20 @@ export function LinkedList() {
     }
     return currNode;
   }
+  /**
+   * updateAPI - service function, that updates api object
+   * with relevant data
+   */
   function updateAPI() {
     api.amount = amount;
     api.head = head;
     api.tail = tail;
   }
+  /**
+   * push - add new node with passed value
+   * in the end of the list
+   * @param {*} value
+   */
   function push(value) {
     if (!tail) {
       head = {
@@ -67,13 +107,24 @@ export function LinkedList() {
       insertAfterNode(tail, value);
     }
   }
+  /**
+   * pop - removes tail element form the list
+   * if it has a tail and return true,
+   * otherwise return false
+   * @return {boolean}
+   */
   function pop() {
     if (!tail) {
       return false;
     }
-    remove(tail);
+    removeNode(tail);
     return true;
   }
+  /**
+   * iterate - iterates through each node of the list
+   * and applies callback function to them
+   * @param {function} fun
+   */
   function iterate(fun) {
     let currNode = head;
     while (currNode !== null) {
@@ -81,14 +132,28 @@ export function LinkedList() {
       currNode = currNode.next;
     }
   }
+  /**
+   * removeItem - removes node with passed value form the list
+   * if such node exists return true, otherwise return false
+   * @param {*} value
+   * @return {boolean}
+   */
   function removeItem(value) {
     const removedNode = find(value);
     if (!removedNode) {
       return false;
     }
-    remove(removedNode);
+    removeNode(removedNode);
     return true;
   }
+  /**
+   * insertAfter - creates a new node with passed value(2nd argument)
+   * after node with another passed value(1nd argument), if such node
+   * exists, and return true. Otherwise return false.
+   * @param {*} target
+   * @param {*} insertable
+   * @return {boolean}
+   */
   function insertAfter(target, insertable) {
     const insertNode = find(target);
     if (!insertNode) {
--- tasks/99_stack/stack.js
+++ tasks/99_stack/stack.js
@@ -1,3 +1,13 @@
+/**
+ * Stack - module that implements stack behaviour.
+ * Return API that contains such methods and properties:
+ * pop() - pop method
+ * push() - push method
+ * isEmpty - false if stack is empty, true otherwise
+ * size - amount of elements in stack
+ * last - value of last pushed element
+ * @return {Object} API
+ */
 export function Stack() {
   const elements = {};
   let size = 0;
@@ -9,11 +19,21 @@ export function Stack() {
     size: 0,
     last: undefined,
   };
+  /**
+   * push - pushes passed value in the end of the stack
+   * @param {*} value
+   */
   function push(value) {
     elements[size++] = value;
     isEmpty = false;
     updateAPIProps();
   }
+  /**
+   * pop - removes last element from the stack
+   * if it is not empty and return removed value.
+   * Otherwise return undefined.
+   * @return {*}
+   */
   function pop() {
     if (size === 0) {
       return;
@@ -26,6 +46,10 @@ export function Stack() {
     updateAPIProps();
     return value;
   }
+  /**
+   * service function, that updates
+   * API with relevant data
+   */
   function updateAPIProps() {
     api.isEmpty = isEmpty;
     api.size = size;
